18f21c6440547241f44950eb0f94669dddda90f4
M Scarborough <32559506+mscarborough-lampdev@users.noreply.github.com>
2020-04-09 10:07:42 -0400

    add basic webapp

---
+++ src/test/java/com/mikescarborough/demos/RewardsTracker/RewardsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikescarborough.demos.RewardsTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
